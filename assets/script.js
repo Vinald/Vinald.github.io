35a478002c55aabde5ac7b15a8785ff8083c8269
vinald <okiror1vinald@gmail.com>
2024-12-07 21:36:34 +0300

    About me -> information update

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,18 @@
+// Close the navbar menu on scroll
+document.addEventListener("scroll", function () {
+    const navbarCollapse = document.querySelector(".navbar-collapse");
+    if (navbarCollapse.classList.contains("show")) {
+        const navbarToggler = document.querySelector(".navbar-toggler");
+        navbarToggler.click();
+    }
+});
+
 // Smooth scroll for navigation
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute("href")).scrollIntoView({
-        behavior: "smooth",
-      });
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth",
+        });
     });
-  });
-  
+});
